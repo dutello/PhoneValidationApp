d50d514ea541cdef2a81922c2f55b2b0c44299c0
Eduardo Tello <dutello@gmail.com>
2025-04-10 12:20:44 +0400

    Initial Commit

---
+++ PhoneValidationApp/PhoneValidationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneValidationAppApp.swift
+//  PhoneValidationApp
+//
+//  Created by Eduardo Tello on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PhoneValidationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhoneValidationAppTests/PhoneValidationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneValidationAppTests.swift
+//  PhoneValidationAppTests
+//
+//  Created by Eduardo Tello on 10/04/2025.
+//
+
+import Testing
+@testable import PhoneValidationApp
+
+struct PhoneValidationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
